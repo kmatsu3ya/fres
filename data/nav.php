c05c313fb57fbba5b8140e17a04fbf82d0e1c2da
Kenta Matsumiya <matsu3ya@gmail.com>
2017-08-09 19:18:47 +0900

    del description etc

---
+++ data/nav.php
@@ -0,0 +1,18 @@
+
+<nav class="top-bar" data-topbar role="navigation">
+    <section class="top-bar-section">
+        <!-- Left Nav Section -->
+        <ul class="left">
+            <li class="has-dropdown">
+                <a href="#">項目</a>
+                <ul class="dropdown">
+                    <li><a href="problem.html">問題をみつける</a></li>
+                    <li><a href="research.html">調査をする</a></li>
+                    <li><a href="hypothesis.html">仮説を立てる</a></li>
+                    <li><a href="experiment.html">実験をする</a></li>
+                    <li><a href="conclusion.html">結論を得る</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+</nav>
